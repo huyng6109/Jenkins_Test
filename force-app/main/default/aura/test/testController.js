d476e8e3f4bc2cd8343b202ed7a9ed913938994e
GiaHuyy <huyid@protonmail.com>
2022-07-27 09:25:30 +0700

    delete

--- force-app/main/default/aura/test/testController.js
+++
@@ -1,8 +0,0 @@
-({
-        clickReimbursed: function(component, event, helper) {
-            let expense = component.get("v.expense");
-            let updateEvent = component.getEvent("updateExpense");
-            updateEvent.setParams({ "expense": expense });
-            updateEvent.fire();
-        }
-})
--- force-app/main/default/triggers/UpdateMailingTrigger.trigger
+++
@@ -1,7 +0,0 @@
-trigger UpdateMailingTrigger on Account (after update) {
-    if (Trigger.isAfter) {
-        if (Trigger.isUpdate){
-            UpdateMailingAddress.updateMailingAddress(Trigger.oldMap, Trigger.new);
-        }
-    }
-}
